5ae6a49872d1fd044453485a4008b6cd73c8e3ab
maslianok <maslianok@gmail.com>
2025-02-22 08:03:46 +0100

    2025.02.22 set trustHost: true

--- auth.config.ts
+++ auth.config.ts
@@ -4,4 +4,5 @@ import type { NextAuthConfig } from 'next-auth';
 // https://authjs.dev/guides/edge-compatibility#middleware
 export default {
   providers: [GitHub],
+  trustHost: true,
 } satisfies NextAuthConfig;

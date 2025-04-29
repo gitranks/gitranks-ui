7937abe9ac403cc7fe56e3f85f60d71d00d19fc8
maslianok <maslianok@gmail.com>
2025-04-29 10:38:52 +0200

    fix footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,17 +1,19 @@
+import { Link } from '../link/link';
+
 export const Footer = () => {
   return (
     <footer className="flex items-center justify-between p-4">
-      <div className="flex items-center gap-2">
-        <a href="https://github.com/gitranks/gitranks-ui" target="_blank" rel="noopener noreferrer">
+      <div className="flex items-center gap-2 text-xs">
+        <Link href="https://github.com/gitranks/gitranks-ui" target="_blank" rel="noopener noreferrer">
           GitHub
-        </a>
+        </Link>
       </div>
-      <p className="text-sm">
-        Hand-coded with ❤️, bugs, and dreams ·{' '}
-        <a href="https://github.com/maslianok" target="_blank" rel="noopener noreferrer">
+      <p className="text-xs">
+        <span className="hidden md:inline">Hand-coded with love, bugs, and dreams by</span>
+        <span className="inline md:hidden">brought to you by</span>{' '}
+        <Link href="https://github.com/maslianok" target="_blank" rel="noopener noreferrer">
           @maslianok
-        </a>{' '}
-        · 2025
+        </Link>
       </p>
     </footer>
   );

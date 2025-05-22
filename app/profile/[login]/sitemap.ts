f702abed7e53315944bd389efcafb77bb0ab39e6
maslianok <maslianok@gmail.com>
2025-05-22 16:35:38 +0200

    update profile sitemap logic

--- app/profile/[login]/sitemap.ts
+++ app/profile/[login]/sitemap.ts
@@ -5,17 +5,17 @@ import { ProfilesForSitemapDocument } from '@/types/generated/graphql';
 import { RankingType } from '@/types/ranking.types';
 
 export default async function sitemap() {
-  const { rankingsForSitemap } = (await graphqlDirect(ProfilesForSitemapDocument)) ?? {};
+  const { profilesForSitemap } = (await graphqlDirect(ProfilesForSitemapDocument)) ?? {};
   const startOfMonthDate = startOfMonth(new Date());
   const rankingTypes: RankingType[] = Object.values(RankingType) as RankingType[];
 
-  return rankingsForSitemap.map((profileRanking) => ({
-    url: `${process.env.NEXT_PUBLIC_URI}/profile/${profileRanking.user?.login}`,
+  return profilesForSitemap.map((profile) => ({
+    url: `${process.env.NEXT_PUBLIC_URI}/profile/${profile.login}`,
     lastModified: startOfMonthDate.toISOString(),
     changeFrequency: 'monthly',
     images: rankingTypes.map(
       (rankingType) =>
-        `${process.env.NEXT_PUBLIC_URI}/api/badge/${profileRanking.user?.login}?rankingType=${rankingType}&amp;template=medium`,
+        `${process.env.NEXT_PUBLIC_URI}/api/badge/${profile.login}?rankingType=${rankingType}&amp;template=medium`,
     ),
     priority: 0.95,
   }));

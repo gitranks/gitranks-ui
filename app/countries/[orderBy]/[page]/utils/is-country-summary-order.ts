0510c115f84cfe7d5cfb855a11bc31a5030e754f
maslianok <maslianok@gmail.com>
2025-08-29 21:21:21 +0200

    fix country order type guard

--- app/countries/[orderBy]/[page]/utils/is-country-summary-order.ts
+++ app/countries/[orderBy]/[page]/utils/is-country-summary-order.ts
@@ -3,5 +3,5 @@ import { CountrySummaryOrder } from '@/types/generated/graphql';
 const ORDER_BY_SET = new Set<string>(Object.values(CountrySummaryOrder));
 
 export function isCountrySummaryOrder(v: unknown): v is CountrySummaryOrder {
-  return typeof v === 'string' && ORDER_BY_SET.has(v);
+  return typeof v === 'string' && ORDER_BY_SET.has(v.toUpperCase());
 }

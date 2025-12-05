ab7e20f9fb02622bbcf9a0dd1ed4f628d43d9b9c
maslianok <maslianok@gmail.com>
2025-12-05 15:49:17 +0100

    remove logs

--- lib/signed-fetch.ts
+++ lib/signed-fetch.ts
@@ -3,7 +3,6 @@
 import jwt from 'jsonwebtoken';
 
 export async function signedFetch(path: string, init: RequestInit = {}) {
-  console.log('JWT', process.env.INTERNAL_JWT_SECRET);
   const token = jwt.sign({}, process.env.INTERNAL_JWT_SECRET!, { expiresIn: '2m' });
 
   const headers = new Headers(init.headers);

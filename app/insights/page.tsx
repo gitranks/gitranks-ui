52e6e364cb221c811325243c3f4feea0882949f0
maslianok <maslianok@gmail.com>
2026-03-22 22:38:24 +0100

    add use cache to the insights page

--- app/insights/page.tsx
+++ app/insights/page.tsx
@@ -1,9 +1,16 @@
+'use cache';
+
+import { cacheLife, cacheTag } from 'next/cache';
+
 import { InsightsList } from './insights-list';
 import { Header } from '@/components/header/header';
 import { Page } from '@/components/page/page';
 import { fetchInsightsServer } from '@/graphql/helpers/fetch-insights-server';
 
 export default async function InsightsPage() {
+  cacheLife('minutes');
+  cacheTag(`insights`);
+
   const { insights = [] } = await fetchInsightsServer({ skip: 0, limit: 20 });
 
   return (
